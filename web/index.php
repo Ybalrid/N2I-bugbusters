c18f7bb5116f29c07a305ae89c26135328c92be0
Mickael <mickael@mayeur.eu>
2015-12-03 22:42:36 +0100

    Init file

--- web/index.php
+++ web/index.php
@@ -1,2 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+
+        <h1><?php echo "Hello nuit de l'info" ?></h1>
+
+        <p>My first paragraph.</p>
+
+    </body>
+</html>
+<script src="myscripts.js"></script>
 <?php
-echo "hello nuit de l'info";
+
