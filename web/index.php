58374b2216fbbcf5ac7477fe11cba2d855f5af7b
Ybalrid <ybalrid@cecmu.org>
2015-12-04 04:51:48 +0000

    add navbar

--- web/index.php
+++ web/index.php
@@ -6,15 +6,44 @@
     </head>
 
     <body>
-        <header>
+    <!---    <header>
             <h1>Crisis Signalement</h1>
         </header>        
+-->
 
+
+			<nav class="navbar navbar-default navbar-fixed-top">
+
+				<div class="navbar-header">
+						<a class="navbar-brand" href="#">BugBusters Crisis Manager</a>
+						<button class="navbar-toggle collapsed" aria-controls="navbar" aria-expanded="false" data-target="#navbarCollapsed" data-toggle="collapse" type="button">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+					</div>
+					<div id="navbarCollapsed" class="collapse navbar-collapse">	
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="#monCarousel">What is BBCM ?</a></li>
+							<li><a href="#alert">Send a signal !</a></li>
+							<li><a href="#map">Watch the map !</a></li>
+							<li><a href="#about_us">About us</a></li>
+						</ul>
+					</div>
+				
+				
+					
+				</div>
+				
+				
+			</div>
+		</nav>
         <?php include ("carousel.php"); ?>
 
         <nav class="sig_buttons">
-            <button class="red" onclick="location.href='./signaler.php'">Signaler un incident</button>
-            <button class="green" onclick="location.href='./safe.php'">Signaler en sécurité</button>
+            <button class="big_button red" onclick="location.href='./signaler.php'">Signaler un incident</button>
+            <button class="big_button green" onclick="location.href='./safe.php'">Signaler en sécurité</button>
         </nav>
 
         <footer>
