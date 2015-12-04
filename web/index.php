5d469517bfc40d359284841a76aa44de5e607263
Ybalrid <ybalrid@cecmu.org>
2015-12-04 07:09:11 +0000

    Add google map

--- web/index.php
+++ web/index.php
@@ -26,7 +26,7 @@
 					<div id="navbarCollapsed" class="collapse navbar-collapse">	
 						<ul class="nav navbar-nav">
 							<li class="active"><a href="#monCarousel">What is BBCM ?</a></li>
-							<li><a href="map.php">Watch the map !</a></li>
+							<li><a href="#map">Watch the map !</a></li>
 							<li><a href="about.php">About us</a></li>
 						</ul>
 					</div>
@@ -44,6 +44,9 @@
             <button class="big_button red" onclick="location.href='./signaler.php'">Signaler un incident</button>
             <button class="big_button green" onclick="location.href='./safe.php'">Signaler en sécurité</button>
         </nav>
+
+
+        <?php include("map.php");?>
 
         <footer>
             <nav>
